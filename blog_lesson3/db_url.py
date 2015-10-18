99418f1daf5cf5f8cb593aee8f78b1b4772c3822
salamer <salameryy@163.com>
2015-10-18 13:32:08 +0800

    ~~~

--- blog_lesson3/db_url.py
+++ blog_lesson3/db_url.py
@@ -22,13 +22,25 @@ class BlogHandler(tornado.web.RequestHandler):
 	def post(self):
 		editor=self.get_argument("editor")
 		body=self.get_argument("body")
-		self.con.insert_one({"editor":editor,"body":body}).inserted_id
+		index=int(self.con.find().count())+1
+		self.con.insert_one({"editor":editor,"body":body,"index":index}).inserted_id
 		self.redirect("/")
+
+class ArchiveHandler(tornado.web.RequestHandler):
+	def initialize(self):
+		self.client=pymongo.MongoClient(Mongo_url,Mongo_port)
+		self.db=self.client[db_name]
+		self.con=self.db[con_name]
+	def get(self,index):
+		index=int(index)
+		blog=self.con.find_one({"index":index})
+		self.render("archive.html",blog=blog)
 
 def init_app():
 	print "app is running on http://127.0.0.1:%s" % port
 	return tornado.web.Application([
-		tornado.web.url(r"/",BlogHandler)
+		tornado.web.url(r"/",BlogHandler),
+		tornado.web.url(r"/archive/(\d+)",ArchiveHandler),
 	])
 
 if __name__=="__main__":
